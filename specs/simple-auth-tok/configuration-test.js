da51a111357e40d50364330a184257bf9d4834ed
Ahmed Hazem <ahazemm@gmail.com>
2014-11-01 14:12:44 +0200

    Add two other configuration options

--- specs/simple-auth-tok/configuration-test.js
+++ specs/simple-auth-tok/configuration-test.js
@@ -1,9 +1,21 @@
 import Configuration from '/simple-auth-tok/configuration';
 
 describe('Configuration', function() {
-  describe('serverTokenEndPoint', function() {
+  describe('serverTokenEndpoint', function() {
     it('defaults to /login', function() {
       expect(Configuration.serverTokenEndpoint).to.eq('/login');
+    });
+  });
+
+  describe('modelName', function() {
+    it('defaults to "user"', function() {
+      expect(Configuration.modelName).to.eq('user');
+    });
+  });
+
+  describe('tokenAttributeName', function() {
+    it('defaults to "token"', function() {
+      expect(Configuration.tokenAttributeName).to.eq('token');
     });
   });
 });
