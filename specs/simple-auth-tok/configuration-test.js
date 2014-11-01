6fb963d2ca9feb2b5f3aa9dcc8faeb0ef8a1fc55
Ahmed Hazem <ahazemm@gmail.com>
2014-11-01 22:28:23 +0200

    Load config like other Ember Simple Auth packages

--- specs/simple-auth-tok/configuration-test.js
+++ specs/simple-auth-tok/configuration-test.js
@@ -18,4 +18,24 @@ describe('Configuration', function() {
       expect(Configuration.tokenAttributeName).to.eq('token');
     });
   });
+
+  describe('.load', function() {
+    it('sets serverTokenEndpoint correctly', function() {
+      Configuration.load(this.container, { serverTokenEndpoint: '/sign_in' });
+
+      expect(Configuration.serverTokenEndpoint).to.eq('/sign_in');
+    });
+
+    it('sets modelName correctly', function() {
+      Configuration.load(this.container, { modelName: 'account' });
+
+      expect(Configuration.modelName).to.eq('account');
+    });
+
+    it('sets tokenAttributeName correctly', function() {
+      Configuration.load(this.container, { tokenAttributeName: 'authenticationToken' });
+
+      expect(Configuration.tokenAttributeName).to.eq('authenticationToken');
+    });
+  });
 });
