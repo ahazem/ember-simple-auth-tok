b7907d01241822d7540f56c81fe021b6733ed5de
Ahmed Hazem <ahazemm@gmail.com>
2014-11-03 23:31:07 +0200

    Add an authorizer

---
+++ specs/simple-auth-tok/authorizers/tok-test.js
@@ -0,0 +1,74 @@
+import Tok from '/simple-auth-tok/authorizers/tok';
+import Session from 'simple-auth/session';
+import EphemeralStore from 'simple-auth/stores/ephemeral';
+import Configuration from '/simple-auth-tok/configuration';
+
+describe('Tok', function() {
+  beforeEach(function() {
+    this.session = Session.create();
+    this.session.setProperties({ store: EphemeralStore.create() });
+
+    this.authorizer = Tok.create();
+    this.authorizer.set('session', this.session);
+
+    this.request = { setRequestHeader: function() {} };
+    sinon.spy(this.request, 'setRequestHeader');
+  });
+
+  describe('initialization', function() {
+    it('assigns tokenAttributeName from configuration', function() {
+      Configuration.tokenAttributeName = 'tokenAttributeName';
+
+      expect(Tok.create().tokenAttributeName).to.eq('tokenAttributeName');
+    });
+
+    afterEach(function() {
+      Configuration.load({}, {});
+    });
+  });
+
+  describe('#authorize', function() {
+    describe('when the session is authenticated', function() {
+      beforeEach(function() {
+        this.authorizer.set('session.isAuthenticated', true);
+      });
+
+      describe('when the session contains non-empty authentication token', function() {
+        beforeEach(function() {
+          this.authorizer.set('session.token', 'secret token');
+        });
+
+        it('adds the authentication token in the request headers', function() {
+          this.authorizer.authorize(this.request, {});
+
+          expect(this.request.setRequestHeader).to.have.been.calledWith('Authorization', 'Token token="secret token"');
+        });
+      });
+
+      describe('when custom token is used', function() {
+        beforeEach(function() {
+          Configuration.tokenAttributeName = 'employee_token';
+
+          this.authorizer = Tok.create();
+        });
+
+        describe('when the session contains non-empty custom token', function() {
+          beforeEach(function() {
+            this.authorizer.set('session', this.session);
+            this.authorizer.set('session.employee_token', 'secret token');
+          });
+
+          it('adds the employee_token in the request headers', function() {
+            this.authorizer.authorize(this.request, {});
+
+            expect(this.request.setRequestHeader).to.have.been.calledWith('Authorization', 'Token employee_token="secret token"');
+          });
+        });
+
+        afterEach(function() {
+          Configuration.load({}, {});
+        });
+      });
+    });
+  });
+});
